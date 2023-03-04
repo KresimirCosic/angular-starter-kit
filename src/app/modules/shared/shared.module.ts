c08502fcb04a4ed2e816a695d16341cf01271bd7
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2023-03-04 14:26:04 +0100

    module structure formatted via prettier, added module structure as example

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedComponentComponent } from './components/shared-component/shared-component.component';
+
+@NgModule({
+  declarations: [SharedComponentComponent],
+  imports: [CommonModule],
+})
+export class SharedModule {}
