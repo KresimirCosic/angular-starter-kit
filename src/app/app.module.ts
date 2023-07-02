86bcdb259a7deee8e3623e731f08bc1a95f4588f
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2023-07-02 18:46:06 +0200

    chore: workspace style addition, prettier configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
-import { AppComponent } from './app.component'
 import { StoreModule } from '@ngrx/store'
+
+import { AppComponent } from '@app/app.component'
 
 @NgModule({
   declarations: [AppComponent],
